9c47385810867f59d6e82844330a1d485d5eead9
bjornpetersson1 <bjornpetersson1@live.se>
2025-09-13 12:21:37 +0200

    Rewrote the code to make it more clean and easy to read

--- Labb_1_Björn_Petersson/Program.cs
+++ Labb_1_Björn_Petersson/Program.cs
@@ -1,80 +1,76 @@
 ﻿// "29535123p48723487597645723645"
-Console.WriteLine("Skriv siffror och tecken, fast mest siffror: ");
-string usrString = Console.ReadLine();
-Console.WriteLine();
-
-static int[] FindCharIndex(string usrString, int start, char key)
+static int[] FindFirstAndLastCharIndexes(string usrString, int start, char key)
 {
-    int[] result = new int[2];
-    result[0] = start;
-    result[1] = -1;
+    int[] results = new int[2];
+    results[0] = start;
+    results[1] = -1;
     if (int.TryParse(usrString[start].ToString(), out _))
     {
-        for (global::System.Int32 i = start + 1; i < usrString.Length; i++)
-        {
-            if (key == usrString[i])
-            {
-                result[1] = i;
-                break;
-            }
-        }        
+        results[1] = usrString.IndexOf(usrString[start], start + 1);
     }
-    return result;
+    return results;
 }
-
-static bool NoAlienCharInbetween(int firstIndex, int lastIndex, string usrString)
+static bool IsNoLetterBetweenDigits(int firstIndex, int lastIndex, string usrString)
 {
     for (global::System.Int32 i = 0; i < lastIndex - firstIndex; i++)
     {
-        if (!int.TryParse(usrString[firstIndex + i].ToString(), out _)) return false;
+        if (!char.IsDigit(usrString[firstIndex + i])) return false;
     }
     return true;
 }
 
-static void PrintColoredNumberSequences(string usrString, List<string> coloredNumbers)
+static string RedNumbersSeperated(int firstIndex, int lastIndex, string usrString)
 {
-    int listIndex = 0;
+    string result = usrString.Substring(firstIndex, lastIndex - firstIndex+1);
+    return result;
+}
+static string[] GrayNumbersSeperated(int firstIndex, int lastIndex, string usrString)
+{
+    string[] results = new string[2];
+    results[0] = usrString.Remove(firstIndex);
+    results[1] = usrString.Remove(0, lastIndex+1);
+    return results;
+}
+
+static void PrintResults(string usrString)
+{
+    List<ulong> coloredNumUlong = new List<ulong>();
+    ulong sum = 0;
+    int ColoredNumListIndex = 0;
     for (global::System.Int32 i = 0; i < usrString.Length; i++)
     {
-        int[] index = FindCharIndex(usrString, i, usrString[i]);
-        if (index[1] == -1) continue;
-        if (index[1] - index[0] < 1) continue;
-        else if (NoAlienCharInbetween(index[0], index[1], usrString))
+        int[] firstAndLastIndexes = FindFirstAndLastCharIndexes(usrString, i, usrString[i]);
+        if (firstAndLastIndexes[1] == -1) continue;
+        else if (IsNoLetterBetweenDigits(firstAndLastIndexes[0], firstAndLastIndexes[1], usrString))
         {
-            coloredNumbers.Add(string.Empty);
-            for (global::System.Int32 k = 0; k < usrString.Length; k++)
-            {
-                if (k < index[0] || k > index[1])
-                {
-                    Console.ForegroundColor = ConsoleColor.Gray;
-                    Console.Write(usrString[k]);
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.Write(usrString[k]);
-                    coloredNumbers[listIndex] += usrString[k];
-                }
-            }
-            listIndex++;
-            Console.WriteLine();
+            coloredNumUlong.Add(0);
+            string redStringNumber = RedNumbersSeperated(firstAndLastIndexes[0], firstAndLastIndexes[1], usrString);
+            coloredNumUlong[ColoredNumListIndex] = ulong.Parse(redStringNumber);
+            ColoredNumListIndex++;
+            string[] grayNumbers = GrayNumbersSeperated(firstAndLastIndexes[0], firstAndLastIndexes[1], usrString);
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write(grayNumbers[0]);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write(redStringNumber);
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine(grayNumbers[1]);
+
         }
         else continue;
     }
+    for (int i = 0; i < coloredNumUlong.Count; i++)
+    {
+        sum += coloredNumUlong[i];
+    }
+    Console.WriteLine();
+    Console.ForegroundColor = ConsoleColor.Gray;
+    if (sum == 0) Console.WriteLine("Woops inga nummer matchade kriterierna");
+    else Console.WriteLine($"Totalsumma: {sum}");
 }
 
-List<string> coloredNumbers = new List<string>();
-List<ulong> coloredNumInt = new List<ulong>();
-ulong sum = 0;
-
-PrintColoredNumberSequences(usrString, coloredNumbers);
-
-for (int i = 0; i < coloredNumbers.Count; i++)
-{
-    coloredNumInt.Add(ulong.Parse(coloredNumbers[i].ToString()));
-    sum += coloredNumInt[i];
-}
+Console.WriteLine("Skriv siffror och tecken, fast mest siffror: ");
+string usrString = Console.ReadLine();
 Console.WriteLine();
-Console.ForegroundColor = ConsoleColor.Gray;
-if (sum == 0) Console.WriteLine("Woops inga nummer matchade kriterierna");
-else Console.WriteLine($"Totalsumma: {sum}");
+
+PrintResults(usrString);
